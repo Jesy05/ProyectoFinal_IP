cd03ec6ebc830d9a7b2ae97391b08f683438e055
ikmarisz <maguillenc@uamv.edu.ni>
2025-07-04 10:12:52 -0600

    Update classUsuario.py

--- clases/classUsuario.py
+++ clases/classUsuario.py
@@ -7,10 +7,11 @@ class Usuario:
         self.nombre = nombre
         self.contra = contra
 
-    def validar_usuario(self):
-        return (
-            isinstance(self.nombre, str) and
-            len(self.nombre) <= 50 and
+    def validar_usuario(self): # Este es un método de instancia (usa self) que normalmente pertenece
+        # a una clase como Usuario. Su propósito es validar si el nombre del usuario es válido.
+        return ( # True si valor es del tipo indicado. False si no lo es.
+            isinstance(self.nombre, str) and # isinstance(self.nombre, str) verifica si self.nombre es una cadena de texto (str).
+            len(self.nombre) <= 50 and # Verifica que el nombre tenga 50 caracteres o menos.
             re.fullmatch(r"[A-Za-z0-9_ ]*", self.nombre) is not None # Acepta letras, números, guiones bajos y espacios
         ) # re.fullmatch asegura que toda la cadena cumpla con el patrón
 
