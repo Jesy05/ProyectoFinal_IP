366506a5356f34ba0183a4447480ed9ca6a69594
alee1616 <maguillenc@uamv.edu.ni>
2025-07-08 14:10:59 -0600

    .

--- clases/classArticulo.py
+++ clases/classArticulo.py
@@ -4,7 +4,7 @@ class Articulo: ## se está declarando una clase llamada Articulo.
     def __init__(self, titulo, autor, contenido, imagen=None): #Este es el método __init__, 
         # también llamado constructor.Se ejecuta automáticamente cuando se crea un 
         # nuevo objeto de la clase Articulo.
-        self.titulo = titulo # # self hace referencia al objeto mismo
+        self.titulo = titulo #  self hace referencia al objeto mismo
         #  crea un atributo llamado titulo y le asigna el valor que llegó como parámetro.
         self.autor = autor # Crea un atributo llamado autor y le asigna el valor que llegó como parámetro.
         # self.autor es el nombre del autor del artículo.
